131dc8a1f90c7609dd9d606ac48e3ebb0e5fdfad
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-10-24 15:07:57 -0500

    add navbar module dropdown

---
+++ src/batting.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include('header.php'); ?>
+    <title>MLB Data - Batting</title>
+</head>
+<body>
+  <?php include('navbar.php'); ?>
+
+  <div class="container">
+    
+    <h1 class="text-center mt-5 mb-5">Batting</h1>
+
+
+
+
+
+
+  </div>
+
+<?php include('footer.php'); ?>
+</body>
+</html>
---
+++ src/pitching.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include('header.php'); ?>
+    <title>MLB Data - Pitching</title>
+</head>
+<body>
+  <?php include('navbar.php'); ?>
+
+  <div class="container">
+    
+    <h1 class="text-center mt-5 mb-5">Pitching</h1>
+
+
+    
+
+
+
+
+
+  </div>
+
+<?php include('footer.php'); ?>
+</body>
+</html>
