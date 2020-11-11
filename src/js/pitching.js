fbf983cd8293a8145d98da69d1ef65e95a593614
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-11-10 19:55:18 -0600

    add pitching module

--- src/js/pitching.js
+++ src/js/pitching.js
@@ -1,88 +1,384 @@
-let URL         = 'https://api.mlb-data.ryanrickgauer.com/main.php/pitching?aggregate=true&perPage=100';
-let sortColumn  = null;
-let sortType    = 'desc';
-const urlParams = new URLSearchParams(window.location.search); 
+let globalVariables  = new GlobalVariables();
+const API            = 'https://api.mlb-data.ryanrickgauer.com/main.php/pitching' + globalVariables.getUrl();
+
+let filterColumns = ['year', 'W', 'L', 'G', 'GS', 'CG', 'SHO', 'SV', 'IPouts', 
+                    'H', 'ER', 'HR', 'BB', 'SO', 'BAOpp', 'ERA', 'IBB', 'WP', 
+                    'HBP', 'BK', 'BFP', 'GF', 'R', 'SH', 'SF', 'GIDP'];
+
+let userFilerColumns = [];
+let filters          = new Filters(globalVariables.filters);
+let emptyRows        = '';
+let pagination       = {
+  current: null,
+  first  : null,
+  last   : null,
+  next   : null,
+};
+
 
 // main
 $(document).ready(function() {
-  setGlobalVariables();
+  setUrlInputValues();
 
-  retrieveData(loadTableData);
+  generateBlankRows();
+  $('.table-pitching tbody').html(emptyRows);
+  getData(API, loadTableData, updatePagination);
+
+  $('.btn-pagination.next').on('click', function() {
+    $('.table-pitching tbody').html(emptyRows);
+    getData(pagination.next, loadTableData, updatePagination);
+  });
+
+  $('.btn-pagination.previous').on('click', function() {
+    $('.table-pitching tbody').html(emptyRows);
+    getData(pagination.previous, loadTableData, updatePagination);
+  });
+
+
+  $('.btn-filter-new').on('click', function() {
+    addNewFilterRow();
+  });
+
+
+  $('.form-filters').on('click', '.btn-filter-delete', function() {
+    deleteFilter(this);
+  });
+
+  $('.btn-filters-apply').on('click', function() {
+    applyFilters();
+  });
+
+  $('.btn-sort-apply').on('click', function() {
+    applySort();
+  });
+
+  $('.select-per-page').on('change', function() {
+    applyPerPage();
+  });
+
+  $('.table-pitching').on('mouseover', '.link-player', function() {
+    showPlayerPopover(this);
+  });
+
+  $('.table-pitching').on('mouseout', '.link-player', function() {
+    removePlayerPopover(this);
+  });
 
 });
 
-function setGlobalVariables() {
-  if (urlParams.has('column'))
-    sortColumn = urlParams.get('column');
-  if (urlParams.has('type') && urlParams.get('type') == 'asc')
-    sortType = 'asc';
-  if (sortColumn != null)
-    URL += '&sort=' + sortColumn + ':' + sortType; 
+
+
+function setUrlInputValues() {
+
+  // set perPage
+  const perPageOptions = $('.select-per-page option');
+  for (let count = 0; count < perPageOptions.length; count++) {
+    if ($(perPageOptions[count]).val() == globalVariables.perPage) {
+      $(perPageOptions[count]).prop('selected', true);
+      break;
+    }
+
+    $(perPageOptions[count]).prop('selected', false);
+  }
+
+  // sort type and column
+  if (globalVariables.sort != null) {
+    // set sort column
+    const inputSortColumns = $('#form-sort-column option');
+    let i = 0;
+    while ($(inputSortColumns[i]).val() != globalVariables.sortColumn) {
+      i++;
+    }
+
+    $(inputSortColumns[i]).prop('selected', true);
+
+    // set sort type
+    if (globalVariables.sortType == 'asc') {
+      $('#form-sort-type-asc').prop('checked', true);
+      $('#form-sort-type-desc').prop('checked', false);
+    } else {
+      $('#form-sort-type-asc').prop('checked', false);
+      $('#form-sort-type-desc').prop('checked', true);
+    }
+  }
+
+
+  // add existing filters to the modal
+  const filterList = filters.filterList;
+  for (let count = 0; count < filterList.length; count++)
+    addNewFilterRow(filterList[count].column, filterList[count].conditional, filterList[count].qualifier);
+
 }
 
 
-function retrieveData(action) {
-  $.getJSON(URL, function(response) {
-    action(response);
+function showPlayerPopover(link) {
+  $('.table-pitching [data-toggle="popover"]').popover('hide');
+  const playerID = $(link).closest('.table-pitching-row').attr('data-player-id');
+  const playerUrl = 'https://api.mlb-data.ryanrickgauer.com/main.php/people/' + playerID;
+
+  $.getJSON(playerUrl, function(response) {
+    // if the mouse is still not hovering over the link exit
+    if (!$(link).is(':hover'))
+      return;
+
+    let content = getPlayerPopoverContent(response);
+    $(link).attr('data-content', content);
+    $(link).popover('show');
+  });
+
+}
+
+function getPlayerPopoverContent(data) {
+
+  let html = `
+  <div class="d-flex align-items-start">
+    <div class="player-bio-left mr-3">
+      <div class="player-bio-item">
+        <img src="${data.results.image}" width="60" height="90" alt="Player image" class="player-item-data image">
+      </div>
+    </div>
+    <div class="player-bio-right">
+      <h4 class="player-bio-item-data name">${data.results.nameFirst} ${data.results.nameLast}</h4>
+      <div class="player-bio-item">
+        <span class="player-bio-item-label">Debut date</span>
+        <span class="player-bio-item-data debut-date">${data.results.debuteDate}</span>
+      </div>
+      <div class="player-bio-item">
+        <span class="player-bio-item-label">Team</span>
+        <span class="player-bio-item-data team">${data.results.team}</span>
+      </div>
+    </div>
+  </div>
+  `;
+
+  return html;
+
+}
+
+function removePlayerPopover(link) {
+  $('.table-pitching [data-toggle="popover"]').popover('hide');
+  // $(link).popover('hide');
+}
+
+function applyPerPage() {
+  let newPerPage = $('.select-per-page option:checked').val();
+  globalVariables.perPage = newPerPage;
+  refreshPage();
+}
+
+
+function updatePagination(newPagination) {
+  pagination.current  = newPagination.current;
+  pagination.first    = newPagination.first;
+  pagination.last     = newPagination.last;
+  pagination.next     = newPagination.next;
+  pagination.previous = newPagination.previous;
+
+  // disable the previous button if previous link is null
+  if (pagination.previous == null) {
+    $('.btn-pagination.previous').prop('disabled', true);
+  } else {
+    $('.btn-pagination.previous').prop('disabled', false);
+  }
+
+}
+
+
+function generateBlankRows() {
+  let html = '';
+
+  for (var count = 0; count < globalVariables.perPage; count++) {
+    html += `
+    <tr>
+      <td colspan="27">
+        <div class="text-center">
+        <div class="spinner-border spinner-border-sm" role="status">
+        <span class="sr-only">Loading...</span>
+        </div></div>
+      </td>
+    </tr>`;
+  }
+
+  emptyRows = html;
+}
+
+
+function getData(url, actionResults, actionPagination) {
+  $.getJSON(url, function(response) {
+    actionResults(response.results);
+    actionPagination(response.pagination);    
+  })
+  .fail(function(response) {
+    console.error(response);
   });
 }
-
 
 function loadTableData(data) {
   let html = '';
   for (var count = 0; count < data.length; count++) 
     html += getTableRowHtml(data[count]);
 
+
+  
+
+
+  // $(html).find('td:contains("null")').replaceWith('<b>NP NUUULL</b>');
+  
+  
   $('.table-pitching tbody').html(html);
+
+
 }
 
-
 function getTableRowHtml(data) {
-  let name = '<a href="player.php?playerID=' + data.playerID + '">' + data.nameFirst + ' ' + data.nameLast + '</a>';
 
-  let BAOpp = '-';
-  if (data.BAOpp != null)
-    BAOpp = data.BAOpp.toFixed(2);
 
-  let ERA = '-';
+  const dataKeys = Object.keys(data);
+  for (let count = 0; count < dataKeys.length; count++) {
+    const thisKey = dataKeys[count];
 
-  if (data.ERA != null) {
-    ERA = (9 * data.ER) / (data.IPouts / 3);
-    ERA = ERA.toFixed(2);
+    if (data[thisKey] == null)
+      data[thisKey] = '-';
   }
 
 
+  let player = `<a data-toggle="popover" data-html="true" data-placement="bottom" 
+  class="link-player"
+   href="player.php?playerID=${data.playerID}">${data.nameFirst} ${data.nameLast}</a>`;
+
   let html = `
-  <tr>
-    <td>${name}</td>
-    <td>${data.years}</td>
-    <td>${data.W}</td>
-    <td>${data.L}</td>
-    <td>${data.G}</td>
-    <td>${data.GS}</td>
-    <td>${data.CG}</td>
-    <td>${data.SHO}</td>
-    <td>${data.SV}</td>
-    <td>${data.IPouts}</td>
-    <td>${data.H}</td>
-    <td>${data.ER}</td>
-    <td>${data.HR}</td>
-    <td>${data.BB}</td>
-    <td>${data.SO}</td>
-    <td>${BAOpp}</td>
-    <td>${ERA}</td>
-    <td>${data.IBB}</td>
-    <td>${data.WP}</td>
-    <td>${data.HBP}</td>
-    <td>${data.BK}</td>
-    <td>${data.BFP}</td>
-    <td>${data.GF}</td>
-    <td>${data.R}</td>
-    <td>${data.SH}</td>
-    <td>${data.SF}</td>
-    <td>${data.GIDP}</td>
-  </tr>`;
+    <tr class="table-pitching-row" data-player-id="${data.playerID}">
+      <td>${player}</th>
+      <td>${data.year}</th>
+      <td>${data.W}</th>
+      <td>${data.L}</th>
+      <td>${data.G}</th>
+      <td>${data.GS}</th>
+      <td>${data.CG}</th>
+      <td>${data.SHO}</th>
+      <td>${data.SV}</th>
+      <td>${data.IPouts}</th>
+      <td>${data.H}</th>
+      <td>${data.ER}</th>
+      <td>${data.HR}</th>
+      <td>${data.BB}</th>
+      <td>${data.SO}</th>
+      <td>${data.BAOpp}</th>
+      <td>${data.ERA}</th>
+      <td>${data.IBB}</th>
+      <td>${data.WP}</th>
+      <td>${data.HBP}</th>
+      <td>${data.BK}</th>
+      <td>${data.BFP}</th>
+      <td>${data.GF}</th>
+      <td>${data.R}</th>
+      <td>${data.SH}</th>
+      <td>${data.SF}</th>
+      <td>${data.GIDP}</th>
+    </tr>`;
 
   return html;
+}
+
+
+function addNewFilterRow(column, conditional, qualifier) {
+  let html = '<div class="input-group input-group-filter">';
+
+  // column
+  html += getFilterColumnOptionsHtml(column);
+
+  // conditional
+  html += `
+    <select class="form-control filter-conditional">
+      <option value="=">=</option>
+      <option value="!=">!=</option>
+      <option value=">=">>=</option>
+      <option value="<="><=</option>
+      <option value=">">></option>
+      <option value="<"><</option>
+    </select>`;
+
+    // set the conditional to selected if it is set
+    if (conditional != undefined)
+      $(html).find('.filter-conditional option[value="' + conditional + '"]').prop('selected', true);
+
+  // qualifier
+  if (qualifier == undefined)
+    html += '<input type="text" class="form-control filter-qualifier">';
+  else
+    html += `<input type="text" class="form-control filter-qualifier" value="${qualifier}">`;
+
+  // delete filter button
+  html += `
+    <div class="input-group-append">
+      <button class="btn btn-outline-secondary btn-filter-delete" type="button"><i class='bx bx-trash'></i></button>
+    </div>`;
+
+  html += '</div>';
+
+  $('.form-filters').append(html);
+}
+
+
+// TODO: don't need to check for used filters
+function getFilterColumnOptionsHtml(existingColumn) {
+  // get a list of already used filters
+  let usedFilters = $('.filter-column option:checked').text();
+  let html        = '<select class="form-control filter-column">';
+
+  for (let count = 0; count < filterColumns.length; count++) {
+
+    if (existingColumn == undefined || existingColumn != filterColumns[count]) {
+      html += `<option value="${filterColumns[count]}">${filterColumns[count]}</option>`;      
+    } else {
+      html += `<option selected value="${filterColumns[count]}">${filterColumns[count]}</option>`;  
+    }
+  }
+  
+  html += '</select>';
+  return html;
+}
+
+//////////////////////////////////////////
+// remove a filter from the filter list //
+//////////////////////////////////////////
+function deleteFilter(btn) {
+  $(btn).closest('.input-group-filter').remove();
+}
+
+
+// Executes when the .btn-filters-apply button is clicked
+function applyFilters() {
+  // reset the filters
+  filters = new Filters();
+
+  let filterRows = $('.input-group-filter');
+
+  for (let count = 0; count < filterRows.length; count++) {
+    let newColumn      = $(filterRows[count]).find('.filter-column option:checked').val();
+    let newConditional = $(filterRows[count]).find('.filter-conditional option:checked').val();
+    let newQualifier   = $(filterRows[count]).find('.filter-qualifier').val();
+    filters.addFilter(newColumn, newConditional, newQualifier);
+  }
+
+  // update the filters
+  globalVariables.filters = filters.getFiltersString();
+
+  refreshPage();
+}
+
+
+function applySort() {
+  let sortColumn       = $('.sort-column option:checked').val();
+  let sortType         = $('input[name="form-sort-type"]:checked').val();
+  let sortResult       = `${sortColumn}:${sortType}`;
+  globalVariables.sort = sortResult;
+
+  refreshPage();
+}
+
+function refreshPage() {
+  // go to the new page
+  window.location.href = window.location.protocol + window.location.pathname + globalVariables.getUrl();
 }
 
--- src/pitching.php
+++ src/pitching.php
@@ -1,75 +1,50 @@
-<?php
-
-require_once('Modules.php');
-require_once('HTML-Generator.php');
-
-?>
-
+<?php require_once('HTML-Generator.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <?php include('header.php'); ?>
-    <title>MLB Data - Pitching</title>
+  <?php include('header.php'); ?>
+  <title>MLB Data - Pitching</title>
 </head>
 <body>
   <?php include('navbar.php'); ?>
 
   <div class="container">
-    
     <h1 class="text-center mt-5 mb-5">Pitching</h1>
 
-    <form class="form-inline">
-      <!-- select -->
-      <select class="form-control mr-4" name="column">
-        <option value="yearID">years</option>
-        <option value="W">W</option>
-        <option value="L">L</option>
-        <option value="G">G</option>
-        <option value="GS">GS</option>
-        <option value="CG">CG</option>
-        <option value="SHO">SHO</option>
-        <option value="SV">SV</option>
-        <option value="IPouts">IPouts</option>
-        <option value="H">H</option>
-        <option value="ER">ER</option>
-        <option value="HR">HR</option>
-        <option value="BB">BB</option>
-        <option value="SO">SO</option>
-        <option value="BAOpp">BAOpp</option>
-        <option value="ERA">ERA</option>
-        <option value="IBB">IBB</option>
-        <option value="WP">WP</option>
-        <option value="HBP">HBP</option>
-        <option value="BK">BK</option>
-        <option value="BFP">BFP</option>
-        <option value="GF">GF</option>
-        <option value="R">R</option>
-        <option value="SH">SH</option>
-        <option value="SF">SF</option>
-        <option value="GIDP">GIDP</option>
-      </select>
-
-      <!-- checkbox -->
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" name="type" value="asc" checked>
-        <label class="form-check-label">Ascending</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" name="type" value="desc">
-        <label class="form-check-label">Descensing</label>
+    <!-- toolbar -->
+    <div class="d-flex align-items-center justify-content-start my-3">
+      <!-- pagination -->
+      <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-primary btn-pagination previous" disabled><</button>
+        <button type="button" class="btn btn-primary btn-pagination next">></button>
       </div>
 
-      <!-- submit -->
-      <button type="submit" class="btn btn-sm btn-primary ml-4">Sort table</button>
-    </form>
+      <!-- filters -->
+      <button type="button" class="btn btn-sm btn-primary ml-2" data-toggle="modal" data-target="#modal-filter-pitching">Filters</button>
 
-    <div class="card card-table mt-5">
+      <!-- sort -->
+      <button type="button" class="btn btn-sm btn-primary ml-2" data-toggle="modal" data-target="#modal-sort-pitching">Sort</button>
+
+      <!-- per page -->
+      <div class="ml-2">
+        <select class="form-control select-per-page">
+          <option value="10">10</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+          <option value="500">500</option>
+          <option value="1000">1000</option>
+        </select>
+      </div>
+    </div>
+
+    <!-- data table -->
+    <div class="card card-table mt-3">
       <div class="card-body">
         <div class="table-responsive">
           <table class="table table-sm table-pitching">
             <thead><tr>
-              <th>Name</th>
-              <th>years</th>
+              <th>Player</th>
+              <th>Year</th>
               <th>W</th>
               <th>L</th>
               <th>G</th>
@@ -97,7 +72,7 @@ require_once('HTML-Generator.php');
               <th>GIDP</th>
             </tr></thead>
             <tbody>
-              <tr><td colspan="27" rowspan="20">
+              <tr><td colspan="21" rowspan="20">
                 <div class="text-center mt-5 mb-5">
                   <?php echo Html::getSpinner() ?>
                 </div>
@@ -108,11 +83,110 @@ require_once('HTML-Generator.php');
       </div>
     </div>
 
-
-
   </div>
 
-<?php include('footer.php'); ?>
-<script src="js/pitching.js"></script>
+  <!-- filter modal -->
+  <div class="modal fade modal-filter" id="modal-filter-pitching" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Filters</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <!-- filter inputs go here -->
+          <form class="form-filters"></form>
+
+          <!-- new filter button -->
+          <button type="button" class="btn btn-sm btn-outline-secondary form-control btn-filter-new mt-3">Add new filter</button>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary btn-filters-apply">Apply filters</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- sort modal -->
+  <div class="modal fade modal-sort" id="modal-sort-pitching" tabindex="-1">
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Sorting</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <!-- filter inputs go here -->
+          <form class="form-sort">
+
+            <div class="form-group">
+              <label for="form-sort-column">Column</label>
+              <select class="form-control sort-column mb-3" id="form-sort-column">
+                <option value="year">Year</option>
+                <option value="W">W</option>
+                <option value="L">L</option>
+                <option value="G">G</option>
+                <option value="GS">GS</option>
+                <option value="CG">CG</option>
+                <option value="SHO">SHO</option>
+                <option value="SV">SV</option>
+                <option value="IPouts">IPouts</option>
+                <option value="H">H</option>
+                <option value="ER">ER</option>
+                <option value="HR">HR</option>
+                <option value="BB">BB</option>
+                <option value="SO">SO</option>
+                <option value="BAOpp">BAOpp</option>
+                <option value="ERA">ERA</option>
+                <option value="IBB">IBB</option>
+                <option value="WP">WP</option>
+                <option value="HBP">HBP</option>
+                <option value="BK">BK</option>
+                <option value="BFP">BFP</option>
+                <option value="GF">GF</option>
+                <option value="R">R</option>
+                <option value="SH">SH</option>
+                <option value="SF">SF</option>
+                <option value="GIDP">GIDP</option>
+              </select>
+            </div>
+            
+
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="form-sort-type" id="form-sort-type-asc" value="asc">
+              <label class="form-check-label" for="form-sort-type-asc">
+                Ascending
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="form-sort-type" id="form-sort-type-desc" value="desc" checked>
+              <label class="form-check-label" for="form-sort-type-desc">
+                Descending
+              </label>
+            </div>
+
+
+          </form>
+
+          <button type="button" class="btn btn-sm btn-outline-primary form-control mt-3 btn-sort-apply">Sort table</button>
+
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+  <?php include('footer.php'); ?>
+
+  <script src="js/Filters.js"></script>
+  <script src="js/Global-Variables.js"></script>
+  <script src="js/pitching.js"></script>
 </body>
 </html>
