73a02c1c91799a56bca48b084583a2cb9abf50af
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-11-16 19:15:13 -0600

    code cleanup

--- src/js/fielding.js
+++ src/js/fielding.js
@@ -3,6 +3,6 @@ const baseApiUrl = 'https://api.mlb-data.ryanrickgauer.com/main.php/fielding';
 const filterColumns = ['teamName', 'year', 'POS', 'G', 'GS', 'InnOuts', 'PO','A','E', 'DP', 'PB', 'WP', 'SB', 'CS', 'ZR'];
 
 $(document).ready(function() {
-  let moduleTest = new Module(datatable, baseApiUrl, filterColumns);
-  moduleTest.init();
+  let modulesClass = new Module(datatable, baseApiUrl, filterColumns);
+  modulesClass.init();
 });
