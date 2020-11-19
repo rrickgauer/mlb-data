e37ba8fa789263076cc284ab65ca18047a13dba3
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-11-18 19:34:22 -0600

    fix search bar

--- src/navbar.php
+++ src/navbar.php
@@ -4,7 +4,7 @@
       <i class='bx bx-menu'></i>
     </button>
 
-    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNavDropdown">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item"><a class="nav-link" id="nav-item-home" href="home.php">Home</a></li>
         <li class="nav-item"><a class="nav-link" id="nav-item-player" href="player.php">Player</a></li>
