063ef6bdf2a7c27d5767e47634b3db12e227349f
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-11-23 12:55:32 -0600

    add cards

--- src/home.php
+++ src/home.php
@@ -8,33 +8,61 @@
   <?php include('navbar.php'); ?>
 
   <div class="container">
-    
+
     <h1 class="text-center mt-5 mb-5">Home</h1>
 
 
-    <div class="card card-table">
-      <div class="card-body">
-        <nav>
-          <div class="nav nav-normal">
-            <a class="nav-link active" data-toggle="tab" href="#nav-table-module" role="tab">Table</a>
-            <a class="nav-link" data-toggle="tab" href="#nav-chart-module" role="tab">Chart</a>
-          </div>
-        </nav>
-        <div class="tab-content mt-3">
-          <!-- table -->
-          <div class="tab-pane fade show active" id="nav-table-module">
-
-
-
-          </div>
-
-          <!-- chart -->
-          <div class="tab-pane fade" id="nav-chart-module">
-            <canvas id="chart-player-module" class="d-none"></canvas>
+      <div class="card-deck card-deck-home">
+        <!-- people -->
+        <div class="card card-home players">
+          <div class="card-body">
+            <h3 class="data">15454</h3>
+            <p class="label">Players</p>
           </div>
         </div>
-      </div>  
-    </div>
+
+        <!-- appearances -->
+        <div class="card card-home teams">
+          <div class="card-body">
+            <h3 class="data">15454</h3>
+            <p class="label">Teams</p>
+          </div>
+        </div>
+
+        <!-- appearances -->
+        <div class="card card-home batting">
+          <div class="card-body">
+            <h3 class="data">15454</h3>
+            <p class="label">Batting records</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card-deck card-deck-home">
+        <!-- appearances -->
+        <div class="card card-home fielding">
+          <div class="card-body">
+            <h3 class="data">15454</h3>
+            <p class="label">Fielding records</p>
+          </div>
+        </div>
+
+        <!-- appearances -->
+        <div class="card card-home pitching">
+          <div class="card-body">
+            <h3 class="data">15454</h3>
+            <p class="label">Pitching records</p>
+          </div>
+        </div>
+
+        <!-- appearances -->
+        <div class="card card-home appearances">
+          <div class="card-body">
+            <h3 class="data">15454</h3>
+            <p class="label">Appearance records</p>
+          </div>
+        </div>
+      </div>
 
 
 
